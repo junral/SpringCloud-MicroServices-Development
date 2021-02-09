f3e8211897007dcac7e439fefd59f7a9ec7ce71b
junral <junral@163.com>
2021-02-09 15:40:17 +0800

    msa-weather-city-server 城市数据API微服务的实现

---
+++ msa-weather-city-server/src/main/java/xyz/junral/spring/cloud/weather/controller/CityController.java
@@ -0,0 +1,23 @@
+package xyz.junral.spring.cloud.weather.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import xyz.junral.spring.cloud.weather.service.CityDataService;
+import xyz.junral.spring.cloud.weather.vo.City;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cities")
+public class CityController {
+
+    @Autowired
+    private CityDataService cityDataService;
+
+    @GetMapping
+    public List<City> listCity() throws Exception {
+        return cityDataService.listCity();
+    }
+}
---
+++ msa-weather-city-server/src/main/java/xyz/junral/spring/cloud/weather/vo/CityList.java
@@ -0,0 +1,26 @@
+package xyz.junral.spring.cloud.weather.vo;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+/**
+ * 城市列表
+ */
+@XmlRootElement(name = "c")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class CityList {
+
+    @XmlElement(name = "d")
+    private List<City> cityList;
+
+    public List<City> getCityList() {
+        return cityList;
+    }
+
+    public void setCityList(List<City> cityList) {
+        this.cityList = cityList;
+    }
+}
