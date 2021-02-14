fcfcd0beb3b7c613e933b7aa35e7897215aa466b
junral <junral@163.com>
2021-02-14 10:26:04 +0800

    hello-world-docker Docker运行微服务

--- msa-weather-report-eureka-feign/src/main/java/xyz/junral/spring/cloud/weather/service/impl/WeatherReportServiceImpl.java
+++ msa-weather-report-eureka-feign/src/main/java/xyz/junral/spring/cloud/weather/service/impl/WeatherReportServiceImpl.java
@@ -3,15 +3,10 @@ package xyz.junral.spring.cloud.weather.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import xyz.junral.spring.cloud.weather.service.CityClient;
 import xyz.junral.spring.cloud.weather.service.WeatherDataClient;
 import xyz.junral.spring.cloud.weather.service.WeatherReportService;
-import xyz.junral.spring.cloud.weather.vo.Forecast;
 import xyz.junral.spring.cloud.weather.vo.Weather;
 import xyz.junral.spring.cloud.weather.vo.WeatherResponse;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * 澶╂皵棰勬姤鏈嶅姟
